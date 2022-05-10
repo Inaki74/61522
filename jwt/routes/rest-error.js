060d69eb6348dfff02165c585785b51bdf83f08b
Marcelo Caiafa <marcelo.caiafa@yahoo.com>
2022-05-10 06:40:10 -0300

    ToDo

--- jwt/routes/rest-error.js
+++
@@ -1,8 +0,0 @@
-// clase custom para try/catch.
-class RestError extends Error{
-    constructor(message, status){
-        super(message);
-        this.status = status;
-    }
-}
-module.exports = RestError;
